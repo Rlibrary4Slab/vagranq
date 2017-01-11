83e8b4fb346b2888ec346a0e2798007d18c66943
lefedoreichmann <asta7799@gmail.com>
2017-01-11 10:35:28 +0000

    csscleaned0111

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,8 +16,8 @@
 //= require jquery_ujs
 //= require jquery-ui.min
 //= require jquery.Jcrop
-//= require twitter/bootstrap
 //= require jquery.infinitescroll
 //= require ckeditor/init
+//= require twitter/bootstrap
 //= require bootstrap
 //= require_tree .
