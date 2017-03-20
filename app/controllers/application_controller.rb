58b2b9fdf6fcf298128e74a38d30e413daccc38c
lefedoreichmann <asta7799@gmail.com>
2017-03-20 22:41:25 +0900

    mineditedonviewtouser0320

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,9 +13,9 @@ class ApplicationController < ActionController::Base
     #def error_404
     # render file: "#{Rails.root}/public/404.html", layout: false, status: 404
     #end
-    rescue_from ActiveRecord::RecordNotFound, with: :render_404
-    rescue_from ActionController::RoutingError, with: :render_404
-    rescue_from Exception, with: :render_500
+    #rescue_from ActiveRecord::RecordNotFound, with: :render_404
+    #rescue_from ActionController::RoutingError, with: :render_404
+    #rescue_from Exception, with: :render_500
     
     
     def render_404
