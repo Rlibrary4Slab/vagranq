a83408e2fd12203d3fc231021786bfd257c040b9
Naofumi Emoto <integral.rabbit@gmail.com>
2017-05-28 04:41:42 +0000

    adding notification_helper, refactoring

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < AuthorizedController
 #class ArticlesController < ApplicationController
+  include NotificationsHelper
   before_action :authenticate_user!, only: [:new,:edit]
   before_action :set_article, only: [ :show,:edit, :update,:destroy, :liking_users,:publish, :draft]
   before_action :correct_user,   only: [:edit, :update]
@@ -204,40 +205,21 @@ class ArticlesController < AuthorizedController
     @page_views = @article.impressionist_count
 
     sum_of_imp = 0
-    article_user = @article.user
-    article_user.articles.each do |article|
+    @article.user.articles.each do |article|
       sum_of_imp += article.impressionist_count
     end
 
-    if @page_views <= 1000              #記事ユーザーの獲得閲覧数
+    if @page_views <= 1000              #記事単体view数
       if @page_views % 100 == 0
-        notification = article_user.notifications.build( user_id: @article.user_id, article_id: @article.id, content: @page_views, category: 4, flag: false)
-        notification.save
-      
-        notification_counts = article_user.notifications.where(flag: false).count
-        send = {note_category: 4, article_title: @article.title, imps: @page_views, notification_counts: notification_counts, flag: false}
-        imp_notification = WebsocketRails.users[@article.user_id]
-        imp_notification.send_message(:notification_event, send)
+        notification_savesend(@article, @page_views, 4)
       end
     else
       if @page_views % 2000 == 0
-        notification = article_user.notifications.build( user_id: @article.user_id, article_id: @article.id, content: @page_views, category: 4, flag: false)
-        notification.save
-      
-        notification_counts = article_user.notifications.where(flag: false).count
-        send = {note_category: 4, article_title: @article.title, imps: @page_views, notification_counts: notification_counts, flag: false}
-        imp_notification = WebsocketRails.users[@article.user_id]
-        imp_notification.send_message(:notification_event, send)
+        notification_savesend(@article, @page_views, 4)
       end
     end
-    if sum_of_imp % 1000 == 0                #記事ユーザーの獲得総閲覧数
-      notification = article_user.notifications.build( user_id: @article.user_id, content: sum_of_imp, category: 5, flag: false)
-      notification.save
-      
-      notification_counts = article_user.notifications.where(flag: false).count
-      send = {note_category: 5, article_title: @article.title, sum_of_imp: sum_of_imp, notification_counts: notification_counts, flag: false}
-      imp_notification = WebsocketRails.users[@article.user_id]
-      imp_notification.send_message(:notification_event, send)
+    if sum_of_imp % 1000 == 0            #総view数
+    notification_savesend(@article, sum_of_imp, 5)
     end
     
   end
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,34 +1,19 @@
 class LikesController < ApplicationController
+    include NotificationsHelper
     def like
         @article = Article.find(params[:article_id])
         like = current_user.likes.build(article_id: @article.id, liked_user_id: @article.user_id)
         like.save
-        notification = @article.user.notifications.build( user_id: @article.user_id, article_id: @article.id, content: like.id ,category: 1, flag: false )
-        notification.save
         
-        notification_counts = @article.user.notifications.where(flag: false).count
-        liked_article_counts = @article.liked.count                                     #counter_cacheの読み出し数が何故か1少ない。countクエリ使うのは効率悪い？
-        total_liked_counts = @article.user.liked.count           #counter_cacheの方が効率が良いのであれば、なんとかこちらにも導入したい。
-            
-        send = {note_category:1, article_title: @article.title, notification_counts: notification_counts, flag: false}
-        liked_notification = WebsocketRails.users[@article.user_id]
-        liked_notification.send_message(:notification_event, send)
+        liked_article_counts = @article.liked.count
+        total_liked_counts = @article.user.liked.count
 
+        notification_savesend(@article, like.id, 1)                                         #新着いいね
         if liked_article_counts % 5 == 0                                                    #記事単体いいね数
-            notification = @article.user.notifications.build( user_id: @article.user_id, article_id: @article.id, content: liked_article_counts ,category:2 , flag: false )
-            notification.save
-            
-            send = {note_category:2, article_title: @article.title, liked_count: liked_article_counts, notification_counts: notification_counts, flag: false }
-            liked_notification = WebsocketRails.users[@article.user_id]
-            liked_notification.send_message(:notification_event, send)
+            notification_savesend(@article, liked_article_counts, 2)
         end    
-        if total_liked_counts % 30 == 0                                                        #記事総合いいね数
-            notification = @article.user.notifications.build( user_id: @article.user_id, article_id: @article.id, content: total_liked_counts ,category:3 , flag: false )
-            notification.save
-        
-            send = {note_category:3, article_title: @article.title, liked_count: total_liked_counts, notification_counts: notification_counts, flag: false }
-            liked_notification = WebsocketRails.users[@article.user_id]
-            liked_notification.send_message(:notification_event, send)
+        if total_liked_counts % 30 == 0                                                     #記事総合いいね数
+            notification_savesend(@article, total_liked_counts, 3)
         end
     end
     
@@ -36,24 +21,17 @@ class LikesController < ApplicationController
         @article = Article.find(params[:article_id])
         like = current_user.likes.find_by(article_id: @article.id)
         like.destroy
+        
         notification = @article.notifications.where(category:1).find_by(content: like.id)
-        if notification.present?
-            notification.destroy
+        notification_destroy(notification)
+        if @article.user.liked.count % 5 == 4                                            #記事総合いいね数
+            notification = @article.notifications.where(category:3).last
+            notification_destroy(notification)
         end
-            if @article.user.liked.count % 5 == 4                                                        #記事総合いいね数
-                notification = @article.notifications.where(category:3).last
-                if notification.present?
-                    notification.destroy
-                end
-            end
-            if @article.liked.count % 30 == 29                                                        #記事総合いいね数
-                notification = @article.notifications.where(category:2).last
-                if notification.present?
-                    notification.destroy
-                end
-            end
-        
-        
+        if @article.liked.count % 30 == 29                                               #記事総合いいね数
+            notification = @article.notifications.where(category:2).last
+            notification_destroy(notification)
+        end
     end
     
 end
