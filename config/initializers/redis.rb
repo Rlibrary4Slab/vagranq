e4934b38252fca8260a56d8f44914f7b55f4dc4b
lefedoreichmann <asta7799@gmail.com>
2017-07-16 16:46:04 +0900

    initialredisdropdowncssunknownbardelete

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,11 +1,8 @@
 require 'redis'
 
 if Rails.env != "production"
-# uri = URI.parse("localhost:6379")
-  uri = URI.parse(ENV["REDIS_DEV"])
- REDIS = Redis.new(host: uri.host, port: uri.port)
+ 
+ REDIS = Redis.new(host: ENV['REDIS_DEV'], port: "6379")
 else
-# uri = URI.parse("localhost:6379")
- uri = URI.parse(ENV["REDIS_PRO"]) 
  REDIS = Redis.new(host: ENV['REDIS_PRO'] ,port: "6379")
 end
