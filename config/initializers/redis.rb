f0ea38863346fdb1ed004011a1c649bdf7a723fa
lefedoreichmann <asta7799@gmail.com>
2017-07-22 12:50:58 +0900

    initial

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,7 +1,7 @@
 require 'redis'
 
 if Rails.env != "production"
-REDIS = Redis.new(host: "localhost", port: "6379") 
+REDIS = Redis.new(host: "52.193.60.42", port: "6379") 
 else
  REDIS = Redis.new(host: ENV['REDIS_PRO'] ,port: "6379")
 end
