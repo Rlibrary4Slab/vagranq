16d2ccf103dca3363ec2ce1b94e9b8ed8f5d5ccc
lefedoreichmann <asta7799@gmail.com>
2017-08-08 21:24:16 +0900

    imagepreviewtextnothing

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -8,6 +8,7 @@ CKEDITOR.editorConfig = function( config )
   // Define changes to default configuration here. For example:
   // config.language = 'fr';
   config.uiColor = '#f5f5f5';
+  config.image_previewText = " ";
 
   /* Filebrowser routes */
   // The location of an external file browser, that should be launched when "Browse Server" button is pressed.
